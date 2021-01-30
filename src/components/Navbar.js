bd432bce516c9a5156a2f93fcd9ea651deff3c6a
Carl Harrison <carlharrison1@live.co.uk>
2021-01-30 12:50:52 +0000

    Navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,57 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components/macro";
 import { Link } from "react-router-dom";
 import { menuData } from "../data/MenuData";
 
 const Nav = styled.nav`
   height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: #000;
+`;
 
-  background: red;
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
 const Logo = styled(Link)`
-  color: #000;
+  ${NavLink}
+  font-style: italic;
 `;
 
 const MenuBars = styled.i``;
 
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+`;
 
 const NavMenuLinks = styled(Link)`
-  color: #fff;
+  ${NavLink}
 `;
 
 const Navbar = () => {
   return (
     <Nav>
-      <Logo to="/">ELIXR</Logo>
+      <Logo to="/">CARL</Logo>
       <MenuBars />
       <NavMenu>
-        {menuData.map((item, index) => {
+        {menuData.map((item, index) => (
           <NavMenuLinks to={item.link} key={index}>
             {item.title}
-          </NavMenuLinks>;
-        })}
+          </NavMenuLinks>
+        ))}
       </NavMenu>
-      <h1>Navbar</h1>
     </Nav>
   );
 };
