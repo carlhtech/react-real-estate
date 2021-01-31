211c8b42e4ec8401936d18d3913fe446738df812
Carl Harrison <carlharrison1@live.co.uk>
2021-01-31 16:48:45 +0000

    Responsive navbar with buttons. Also added global styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled, { css } from "styled-components/macro";
 import { Link } from "react-router-dom";
 import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
+import Bars from "../images/bars.svg";
 
 const Nav = styled.nav`
   height: 60px;
@@ -11,7 +13,7 @@ const Nav = styled.nav`
   z-index: 100;
   position: fixed;
   width: 100%;
-  background: #000;
+  background: red;
 `;
 
 const NavLink = css`
@@ -29,21 +31,51 @@ const Logo = styled(Link)`
   font-style: italic;
 `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled.i`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    background-image: url(${Bars});
+    background-size: contain;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    right:0;
+    transform: translate(-50%, 25%);
+  }
+`;
 
 const NavMenu = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const NavMenuLinks = styled(Link)`
   ${NavLink}
 `;
 
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
 const Navbar = () => {
   return (
     <Nav>
-      <Logo to="/">CARL</Logo>
+      <Logo to="/">ELIXR</Logo>
       <MenuBars />
       <NavMenu>
         {menuData.map((item, index) => (
@@ -52,6 +84,11 @@ const Navbar = () => {
           </NavMenuLinks>
         ))}
       </NavMenu>
+      <NavBtn>
+        <Button to="/contact" primary="true">
+          Contact Us
+        </Button>
+      </NavBtn>
     </Nav>
   );
 };
